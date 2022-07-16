ad6563317d337a6c9e5db5ad657fba013769f9c1
Zhenzhao Tu <tuzhenzhao2020@outlook.com>
2022-07-16 18:04:21 -0500

    Version 0.0.1

---
+++ homebrew-weather_zsh.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewWeatherZsh < Formula
+  desc "This is a weather shell script for you to check weather's info of any city you want."
+  homepage ""
+  url "https://github.com/Tu-Zhenzhao/homebrew-weather_zsh/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "f148ff0604859d219812111a92f691dbb9722d8d56f5eb147e70ed47b4199acc"
+  license "MIT"
+
+  depends_on "jq"
+
+  def instal
+    bin.install 'weather'
+    prefix.install "README.md"
+    prefix.install "LICENSE"
+  end
+end
