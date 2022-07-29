a7a554dd899126284ff9ad7e2a914e3e352ed930
Zhenzhao Tu <tuzhenzhao2020@outlook.com>
2022-07-29 00:42:31 -0500

    v0.3.0 new version

--- homebrew-weather_zsh.rb
+++ homebrew-weather_zsh.rb
@@ -4,8 +4,8 @@
 class HomebrewWeatherZsh < Formula
   desc "This is a weather shell script for you to check weather's info of any city you want."
   homepage ""
-  url "https://github.com/Tu-Zhenzhao/homebrew-weather_zsh/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "06c877b06ee5460cea2ab8c797853fdaf44fdda00177de0554abade04c0484d1"
+  url "https://github.com/Tu-Zhenzhao/homebrew-weather_zsh/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "91f2a7f2f8dee3ba485ede1fcf30e3a593b2231a10ef06414483f4afc99c82e7"
   license "MIT"
 
   depends_on "jq"
@@ -15,4 +15,5 @@ class HomebrewWeatherZsh < Formula
     prefix.install "README.md"
     prefix.install "LICENSE"
   end
+
 end
